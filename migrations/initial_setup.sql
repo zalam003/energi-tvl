739c8832fbbe0569d74ed08cf7cebb8edccf6da3
zalam003 <zaki.alam@gmail.com>
2024-05-30 17:37:12 -0400

    update schema

--- migrations/initial_setup.sql
+++ migrations/initial_setup.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS token_data (
     id SERIAL PRIMARY KEY,
-    token_name VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-    total_supply DECIMAL NOT NULL,
-    usd_price DECIMAL NOT NULL,
-    usd_value DECIMAL NOT NULL
+    token_name VARCHAR(255),
+    date DATE,
+    total_supply NUMERIC,
+    usd_price NUMERIC,
+    usd_value NUMERIC,
+    UNIQUE(token_name, date)
 );
 
 CREATE TABLE IF NOT EXISTS tvl_data (
     id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    total_value_locked DECIMAL NOT NULL
+    date DATE UNIQUE,
+    total_value_locked NUMERIC
 );
-
