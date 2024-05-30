c78956ec318497365fe5605bdb5e211c99995212
zalam003 <zaki.alam@gmail.com>
2024-05-30 17:19:20 -0400

    update db on vercel

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 });
 
 module.exports = pool;
-
