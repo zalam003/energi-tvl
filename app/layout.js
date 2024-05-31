af1c2b52554d1f5f2216eb5d8a6ce849f372a729
zalam003 <zaki.alam@gmail.com>
2024-05-31 09:53:35 -0400

    add speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
