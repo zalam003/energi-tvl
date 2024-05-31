fb03648efbf3ab0bf1d57d8bcef9bbd57aaf46ee
zalam003 <zaki.alam@gmail.com>
2024-05-31 09:52:17 -0400

    add speed insights

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
